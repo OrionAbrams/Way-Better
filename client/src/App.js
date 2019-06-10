b2fe696cff80783e5501d14fdb01fe551f7e1df9
tonaramos <gtonaramos@gmail.com>
2019-06-09 17:52:04 -0700

    Delete unused files and added Jesse image.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Destination_Form from "./pages/Destination_Form/Destination_Form";
 import Options from "./pages/Options.js"
-import NoMatch from "./pages/NoMatch";
 // import "./components/style.css";
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
         <Switch>
         <Route exact path="/" component={Destination_Form} />
         {/* <Route exact path="/Options" component={Options} /> */}
-        <Route exact path="/options" component={Options} />
-        <Route component={NoMatch} />
+        <Route exact path="/Options" component={Options} />
         </Switch>
       </div>
     </Router>
--- client/src/pages/Destination_Form/Destination_Form.jsx
+++ client/src/pages/Destination_Form/Destination_Form.jsx
@@ -36,12 +36,13 @@ class Destination_Form extends React.Component {
 
   render() {
     return (
-      <div >
+      <div>
         <div class="navigationBar">
-          Navigation Bar
+          <div class="userImage">
+
+          </div>
         </div>
         <div class="map">
-          Navigation Map
         </div>
         <div class="formSection aqua-gradient">
           <div class="formFrame">
@@ -55,7 +56,7 @@ class Destination_Form extends React.Component {
                 <input size="30" name="destination" className="form-control" placeholder="Where to?" value={this.state.destination} onChange={this.handleInputChange} />
               </div>
               <Link to="/Options" >
-              <button class="btn btn-success" onClick={this.handleFormSubmit} type="submit" className="btn btn-lg btn-danger float-right">
+              <button onClick={this.handleFormSubmit} type="submit" className="btn btn-lg btn-light float-right">
                 Let's go!
               </button> 
               </Link>
